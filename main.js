8397a4c74768c1e03e352489a256dcde6317d0ac
impressao recordalbum <junior.oliveira05@icloud.com>
2024-01-19 09:01:17 -0300

    add buscar()

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 async function poke() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=14');
-  let data = await response.json()
 
-  data.results.forEach(async element => {
-    const resp = await fetch(element.url);
-    const poke = await resp.json();
+  try {
+    const tipoPokemonSelect = document.getElementById('tipoSelector');
 
-    const img = document.createElement('img');
-    const div = document.createElement('div');
-    const caminho = img.src = poke.sprites.front_default;
-    let nome = poke.name
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=14`);
+    let data = await response.json()
 
-    let types = poke.types.map((ele) => ele.type.name);
+    //console.log(data)
 
-    div.innerHTML += `
+    data.results.forEach(async element => {
+      const resp = await fetch(element.url);
+      const poke = await resp.json();
+
+      /*const img = document.createElement('img');
+      const div = document.createElement('div');
+      const caminho = img.src = poke.sprites.front_default;
+      let nome = poke.name
+
+      let types = poke.types.map((ele) => ele.type.name);
+
+      div.innerHTML += `
       <h1>${nome}</h1>
       <img src="${caminho}" alt="${nome}">
       <p>Types: ${types}</p>
       
       `
-    teste.appendChild(div);
+      teste.appendChild(div);*/
 
-  });
+    });
+  } catch (error) {
+    alert("Erro", error)
+  }finally {
+
+  }
 
 }
-
 
 poke();
 
@@ -44,6 +54,8 @@ async function obterInfo() {
     });
 
   } catch (error) {
+    alert('Erro', error)
+  } finally {
 
   }
 }
@@ -52,17 +64,41 @@ async function obterInfo() {
 obterInfo()
 
 async function buscar() {
-  const tipoPokemonSelect = document.getElementById('tipoSelector');
-  console.log(tipoPokemonSelect.value)
 
-  const response = await fetch(`https://pokeapi.co/api/v2/type/` + tipoPokemonSelect.value);
-  let data = await response.json()
+  try {
+    const tipoPokemonSelect = document.getElementById('tipoSelector');
+    //console.log(tipoPokemonSelect.value)
 
-  console.log(data)
+    const response = await fetch(`https://pokeapi.co/api/v2/type/` + tipoPokemonSelect.value);
+    let data = await response.json()
 
-  const img = document.createElement('img');
-  const div = document.createElement('div');
-  //const caminho = img.src = poke.sprites.front_default;
-  let nome = data.name
-   console.log(nome)
+    data.pokemon.forEach(async ele => {
+      //console.log(ele)
+
+      const resp = await fetch(ele.pokemon.url);
+      const poke = await resp.json();
+
+      //console.log(poke)
+
+      const img = document.createElement('img');
+      const div = document.createElement('div');
+      const caminho = img.src = poke.sprites.front_default;
+      let nome = poke.name
+
+      let types = poke.types.map((ele) => ele.type.name);
+
+      div.innerHTML += `
+      <h1>${nome}</h1>
+      <img src="${caminho}" alt="${nome}">
+      <p>Types: ${types}</p>
+      
+      `
+      teste.appendChild(div);
+    })
+  } catch (error) {
+    alert('Erro', error)
+  } finally {
+
+  }
+
 }
